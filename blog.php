9e4495ab7e621a6ff202916d594c97e0589dd7e1
songlm <songlm@songlm-K50IN.(none)>
2012-09-15 17:49:45 +0800

    错误更改，发现自己差的还太多

--- blog.php
+++ blog.php
@@ -7,17 +7,17 @@ $password="6515" ;        //登录数据库的密码
 $my_connect=mysql_connect($host,$user,$password);  //打开数据库
 if($my_connect)
          {
-         echo "成功<p>";
-         mysql_select_db("MY_blog",$my_connect);
-         $sql="select * from blog2012";
-         $result=mysql_query($sql,$my_connent);
-         print_r(mysql_fetch_array($result));     
-         mysql_close($my_connect);  //关闭数据库
+         echo "数组库连接成功";                      //数据库连接成功提示
+         mysql_select_db("MY_blog",$my_connect);    //选择数据库
+         $sql="select * from blog20122";            //SQL语句
+         $result=mysql_query($sql,$my_connect);     //执行SQL语句，之前把$my_connect写成了$my_connnt了，要细心了
+         echo mysql_error();                        //SQL语句执行错误提示
+         print_r(mysql_fetch_array($result));      //把读出来的数据信息（数组）显示出来
+         
+         mysql_close($my_connect);                 //关闭数据库
    
           }
-         else{echo "fail";}
-
-echo var_dump($_GET)."<p>";
+         else{echo "数据库连接失败";}              //数据库连接错误提示
 
 
 
